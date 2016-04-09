67e332673cae172652e5d858e65a7f2b1fd96780
fusion809 <brentonhorne77@gmail.com>
2016-04-09 17:36:50 +1000

    updating scripts

--- Shell/cd.sh
+++ Shell/cd.sh
@@ -6,10 +6,30 @@ function cdgs {
 	cd ~/GitHub/gentoo-scripts
 }
 
+function cdp {
+	cd ~/Programs
+}
+
 function cdsh {
 	cd ~/Shell
 }
 
 function cdst {
 	cd ~/GitHub/sabayon-tools
+}
+
+function cddo {
+	cd ~/Downloads
+}
+
+function cddc {
+	cd ~/Documents
+}
+
+function cdap {
+	cd ~/.local/share/applications
+}
+
+function cddd {
+	cd ~/.local/share/desktop-directories
 }
