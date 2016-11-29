f16fe573ffb2a22329f5a0bbf593fe36d2c00d78
fusion809 <brentonhorne77@gmail.com>
2016-11-29 18:25:05 +1000

    Updating scripts

--- Shell/06-atom.sh
+++ Shell/06-atom.sh
@@ -2,7 +2,7 @@ set -u
 
 export ATOM_VERSION=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest" -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f4 | cut -d '/' -f 8 | sed 's/v//g')
 
-if ! [[ -f $HOME/Programs/AppImages/Atom_${ATOM_VERSION}.glibc2.14-x86_64.AppImage ]]; then
+if ! [[ -f $HOME/Programs/AppImages/Atom-${ATOM_VERSION}.glibc2.14-x86_64.AppImage ]]; then
 	printf "Atom-${ATOM_VERSION}.glibc2.14-x86_64.AppImage does not exist locally! Downloading...\n"
 	if ! [[ -d $HOME/Programs/AppImages ]]; then
 		mkdir -p $HOME/Programs/AppImages
@@ -24,9 +24,10 @@ function edsh {
 	atom $HOME/Shell
 }
 
-MOUNT_LOCATION=/tmp/$(find /tmp/.mount* -name "atom.png" | head -n 1 | cut -d '/' -f 3)
-
-PATH=$PATH:$MOUNT_LOCATION/usr/bin
+if `find /tmp/.mount* -name "atom.png" > /dev/null 2>&1`; then
+	MOUNT_LOCATION=/tmp/$(find /tmp/.mount* -name "atom.png" | head -n 1 | cut -d '/' -f 3)
+	PATH=$PATH:$MOUNT_LOCATION/usr/bin
+fi
 
 function apmi {
 	apm install "$@"
