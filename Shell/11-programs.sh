1211b74cf8e31122ed8580b9db005e493d5bcbaf
fusion809 <brentonhorne77@gmail.com>
2017-03-12 02:38:17 +1000

    Adding hub inst

--- Shell/11-programs.sh
+++ Shell/11-programs.sh
@@ -2,6 +2,10 @@ if ! [[ -f /usr/bin/pastebinit ]]; then
 	emin pastebinit
 fi
 
+if ! [[ -f /usr/bin/hub ]]; then
+	emin dev-vcs/hub
+fi
+
 function pit {
 	pastebinit -i "$@" -b http://paste2.org
 }
