9f0a82f73afbf4227e09a8d74df2866a3a5e10ac
fusion809 <brentonhorne77@gmail.com>
2017-03-13 12:03:03 +1000

    Fixing vimup script

--- Shell/04-ebuild.sh
+++ Shell/04-ebuild.sh
@@ -1,3 +1,23 @@
 function manif {
 	ebuild *.ebuild manifest
 }
+
+function vimup {
+	pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
+	pushd $GHUBM/packaging/vim-overlay/app-editors/vim
+	lver_vim=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 2 | sed 's/\.ebuild//g')
+	mv vim-$lver_vim.ebuild vim-$pkgver.ebuild
+	sudo ebuild vim-$pkgver.ebuild manifest merge
+	popd
+	pushd $GHUBM/packaging/vim-overlay/app-editors/vim-core
+	lver_vimc=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 3 | sed 's/\.ebuild//g')
+	mv vim-core-$lver_vimc.ebuild vim-core-$pkgver.ebuild
+	sudo ebuild vim-core-$pkgver.ebuild manifest merge
+	popd
+	pushd $GHUBM/packaging/vim-overlay/app-editors/gvim
+	lver_gvim=$(ls | grep ebuild | sort -u | tail -n 2 | head -n 1 | cut -d '-' -f 2 | sed 's/\.ebuild//g')
+	mv gvim-$lver_gvim.ebuild gvim-$pkgver.ebuild
+	sudo ebuild gvim-$pkgver.ebuild manifest merge
+	push "Bumping version to $pkgver"
+	popd
+}
