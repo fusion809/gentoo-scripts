866686acc4ac71527c42742048c2b44ed4c58fc6
Brenton Horne <brentonhorne77@gmail.com>
2017-03-17 23:46:07 +1000

    Updating scripts

--- root/Shell/pms/kernel.sh
+++ root/Shell/pms/kernel.sh
@@ -4,4 +4,7 @@ function kern-build {
 	make modules_install
 	make install
 	emmr
+	KERN=$(ls -ld /usr/src/linux | cut -d ' ' -f 11 | sed 's/linux\-//g')
+	dracut -f --fstab --xz /boot/initramfs-$KERN.img $KERN
+	cp /boot/*$KERN* /boot/efi
 }
