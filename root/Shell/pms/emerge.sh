c0ba06a407b9904c1e6c0d13cffc7df0d3f245c8
Brenton Horne <brentonhorne77@gmail.com>
2017-03-17 16:30:38 +1000

    Splitting emerge script into smaller subscripts

--- root/Shell/pms/emerge.sh
+++ root/Shell/pms/emerge.sh
@@ -1,31 +1,4 @@
-function ems {
-	emerge --sync
-}
-
-function sync {
-	emerge --sync && layman -S
-}
-
-function syncup {
-	sync && emerge -uDU --with-bdeps=y @world
-}
-
-function emup {
-	emerge -uDU --with-bdeps=y @world
-}
-
-function emin {
-	emerge -av $@
-}
-
-function emrm {
-	emerge -cav $@
-}
-
-function emaw {
-	emerge --autounmask-write "$@"
-}
-
-function empr {
-	emerge @preserved-rebuild
-}
+for i in $HOME/Shell/pms/emerge/*.sh
+do
+	. "$i"
+done
