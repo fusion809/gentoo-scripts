c77f457be3a304e8d848e0e58565a34c32972744
Brenton Horne <brentonhorne77@gmail.com>
2017-03-18 17:16:54 +1000

    Adding vpu, vpm and vmc

--- root/Shell/vim.sh
+++ root/Shell/vim.sh
@@ -8,6 +8,18 @@ function vbash {
 	vim $HOME/.bashrc
 }
 
+function vmc {
+	vim /etc/portage/make.conf
+}
+
+function vpm {
+	vim /etc/portage/package.mask
+}
+
+function vpu {
+	vim /etc/portage/package.use
+}
+
 function vsh {
 	pushd $HOME/Shell
 	vim
