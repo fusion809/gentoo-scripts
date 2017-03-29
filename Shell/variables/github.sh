c4376676f60abab05cee3df97230e1cb35735f48
Brenton Horne <brentonhorne77@gmail.com>
2017-03-29 20:09:44 +1000

    FC->GC; FS->GS

--- Shell/variables/github.sh
+++ Shell/variables/github.sh
@@ -8,9 +8,8 @@ export SCR=$GHUBM/scripts
 export WEB=$GHUBM/websites
 
 export FGI=$WEB/fusion809.github.io
-export FC=$CFG/funtoo-config
-export FS=$SCR/funtoo-scripts
-export GS=$SCR/gentoo-scripts-config-old
+export GC=$CFG/gentoo-config
+export GS=$SCR/gentoo-scripts
 export ST=$PKG/sabayon-tools
 export VI=$ED/vim
 export VM=$PKG/fusion809-overlay
