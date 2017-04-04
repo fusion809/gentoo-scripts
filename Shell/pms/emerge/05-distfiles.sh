dba9562084ef3d98c95486e5b866edde18841738
Brenton Horne <brentonhorne77@gmail.com>
2017-04-04 17:22:49 +1000

    Adding diss to show distfile dir size

--- Shell/pms/emerge/05-distfiles.sh
+++ Shell/pms/emerge/05-distfiles.sh
@@ -1,7 +1,12 @@
-function rmd {
-    sudo rm /usr/portage/distfiles/"$1"
+function diss {
+    du -sh /usr/portage/distfiles
 }
 
 function ldis {
     ls /usr/portage/distfiles
 }
+
+function rmd {
+    sudo rm /usr/portage/distfiles/"$1"
+}
+
