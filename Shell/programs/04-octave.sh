51e407671c9e2ea72721a5ebdadf1b001d04d86f
fusion809 <fusion809@users.noreply.github.com>
2017-05-24 03:05:21 +1000

    Adding extra calc funcs

--- Shell/programs/04-octave.sh
+++ Shell/programs/04-octave.sh
@@ -9,3 +9,15 @@ function octe {
 function ctof {
     octe "$1*1.8+32"
 }
+
+function ftoc {
+    octe "($1-32)/1.8"
+}
+
+function kgtolb {
+    octe "$1*2.20462"
+}
+
+function lbtokg {
+    octe "$1/2.20462"
+}
