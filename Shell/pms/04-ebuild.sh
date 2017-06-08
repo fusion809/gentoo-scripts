81f3e68ba3a7d10d109aa9a08af191d9c3f22a90
fusion809 <fusion809@users.noreply.github.com>
2017-06-09 03:10:48 +1000

    Oops Manifest needs to be del too in bravup

--- Shell/pms/04-ebuild.sh
+++ Shell/pms/04-ebuild.sh
@@ -132,7 +132,7 @@ function bravup {
     CBRAVE_VER=$(wget -q https://github.com/brave/browser-laptop/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/[a-z]//g' | sed 's/\.*$//g' | sed 's/\.tar\.gz//g')
 
     mv brave-bin-${PBRAVE_VER}.ebuild brave-bin-${CBRAVE_VER}.ebuild
-    sudo rm /usr/portage/distfiles/Brave.tar.bz2
+    sudo rm /usr/portage/distfiles/Brave.tar.bz2 Manifest
     sudo ebuild brave-bin-${CBRAVE_VER}.ebuild manifest merge
     push "[www-client/brave-bin] Bumping to ${CBRAVE_VER}"
 }
