d2d639e5e08fc2e301ca3b346267f42ba6e63f01
fusion809 <fusion809@users.noreply.github.com>
2017-06-08 23:57:59 +1000

    Fixing some weather funcs

--- Shell/programs/11-weather.sh
+++ Shell/programs/11-weather.sh
@@ -5,19 +5,19 @@ function weathv {
 alias weather=weathv
 
 function weathd {
-    weathv | head -n "$1" | tail -n 1 | sed 's/[-\.\ \:\_\;]//g'
+    weathv | head -n "$1" | tail -n 1 | sed 's/[-\.\ \:\/\_\;]//g'
 }
 
 alias weatherd=weathd
 
 function temp {
-    weathd 3
+    weathd 4
 }
 
 alias temperature=temp
 
 function cond {
-    weathd 4
+    weathd 3
 }
 
 alias conditions=cond
