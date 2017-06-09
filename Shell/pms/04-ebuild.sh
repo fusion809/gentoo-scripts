61cd4556188246bfed24e2aeba367cee6be65429
fusion809 <fusion809@users.noreply.github.com>
2017-06-10 07:32:10 +1000

    Adjusting bravup so that rm Manifest goes without confirmation

--- Shell/pms/04-ebuild.sh
+++ Shell/pms/04-ebuild.sh
@@ -176,7 +176,7 @@ function bravup {
 
     if ! [[ $PBRAVE_VER == $CBRAVE_VER ]] && `echo $BRAVE_TAR > /dev/null 2>&1`; then
          mv brave-bin-${PBRAVE_VER}.ebuild brave-bin-${CBRAVE_VER}.ebuild
-         rm Manifest
+         sudo rm -rf Manifest
          sudo ebuild brave-bin-${CBRAVE_VER}.ebuild manifest merge
          push "[www-client/brave-bin] Bumping to ${CBRAVE_VER}"
     else
