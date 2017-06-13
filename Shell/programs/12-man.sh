7bc5e7de24558735493889db95278dde34d2f766
fusion809 <fusion809@users.noreply.github.com>
2017-06-14 01:22:52 +1000

    Adding manhtmlg func

--- Shell/programs/12-man.sh
+++ Shell/programs/12-man.sh
@@ -1,18 +1,19 @@
 function manhtml {
-    if ! [[ -n $3 ]]; then
-         bzcat /usr/share/man/man$2/$1.$2.bz2 | man2html >> $WEB/man/$1.$2.html
-    else
-         bzcat /usr/share/man/man$2/$1.$2.bz2 | man2html | tee $WEB/man/$1.$2.html
-    fi
+    bzcat /usr/share/man/man${2//[a-z]}/$1.$2.bz2 | man2html >> $WEB/man/$1.$2.html
+    sudo chmod 777 $WEB/man/$1.$2.html
 }
 
+function manhtmlg {
+    bzcat /usr/share/gcc-data/x86_64-pc-linux-gnu/6.3.0/man/man${2}/$1.$2.bz2 | man2html >> $WEB/man/$1.$2.html
+    sudo chmod 777 $WEB/man/$1.$2.html
+}
 function manhtmlp {
     L=$(equery files "$1" | grep /usr/share/man/man | grep "bz2")
     for i in $L
     do
-        # echo "$i\n"
-         NAME=$(echo $i | cut -d '/' -f 2 | sed 's/\.[0-9]\.bz2//g')
-         NO=$(echo $i | sed 's/[a-z]//g' | cut -d '/' -f 1)
+         dNO=$(grep -o "\." <<< "$i" | wc -l)
+         NAME=$(echo $i | cut -d '/' -f 6 | sed 's/\.[0-9][a-z]*\.bz2//g')
+         NO=$(echo $i | sed 's/\.bz2//g' | cut -d '/' -f 6 | cut -d '.' -f ${dNO})
          manhtml $NAME $NO
     done
 }
