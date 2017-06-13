573dae79d51a3e6f3f496e87f80bddc5355c3b87
fusion809 <fusion809@users.noreply.github.com>
2017-06-13 21:44:44 +1000

    Updating manhtml func to allow silence by default

--- Shell/programs/12-man.sh
+++ Shell/programs/12-man.sh
@@ -1,3 +1,7 @@
 function manhtml {
-    bzcat /usr/share/man/man$2/$1.$2.bz2 | man2html | tee $WEB/man/$1.$2.html
+    if ! [[ -n $3 ]]; then
+         bzcat /usr/share/man/man$2/$1.$2.bz2 | man2html >> $WEB/man/$1.$2.html
+    else
+         bzcat /usr/share/man/man$2/$1.$2.bz2 | man2html | tee $WEB/man/$1.$2.html
+    fi
 }
