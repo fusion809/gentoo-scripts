d8b34af988a9b714135c54ab35361d587089302f
fusion809 <fusion809@users.noreply.github.com>
2017-06-24 06:35:56 +1000

    Adding a little ammendment to GRUB funcs to counteract Arch boot issue

--- Shell/programs/06-grub.sh
+++ Shell/programs/06-grub.sh
@@ -1,3 +1,4 @@
 function update-grub {
     sudo grub-mkconfig -o /boot/grub/grub.cfg
+    sudo sed -i -e 's|UUID=ad4103fa-d940-47ca-8506-301d8071d467|/dev/sda3|g' /boot/grub/grub.cfg
 }
--- root/Shell/09-grub.sh
+++ root/Shell/09-grub.sh
@@ -1,3 +1,4 @@
 function update-grub {
-	grub-mkconfig -o /boot/grub/grub.cfg
+    grub-mkconfig -o /boot/grub/grub.cfgi
+    sed -i -e 's|UUID=ad4103fa-d940-47ca-8506-301d8071d467|/dev/sda3|g' /boot/grub/grub.cfg
 }
