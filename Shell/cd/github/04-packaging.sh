06dfde2fa7f11bc06a89bde042009690307c0332
fusion809 <fusion809@users.noreply.github.com>
2017-06-24 00:05:26 +1000

    Adding AppImages related cd funcs

--- Shell/cd/github/04-packaging.sh
+++ Shell/cd/github/04-packaging.sh
@@ -2,6 +2,14 @@ function cdpk {
     cdgm "packaging/$1"
 }
 
+function cdaim {
+    cdpk "AppImages/$1"
+}
+
+function cdgam {
+    cdpk "GVim.AppImage/$1"
+}
+
 function cdst {
     cdpk "sabayon-tools/$1"
 }
