213c9d3c758591450201fb4a8ab8201cd97db2a9
fusion809 <fusion809@users.noreply.github.com>
2017-11-06 23:08:55 +1000

    Adding editing funcs and adding optional input to 'em

--- Shell/gvim/03-github.sh
+++ Shell/gvim/03-github.sh
@@ -1,15 +1,22 @@
 function gvcs {
-    gvd "$SCR/centos-scripts"
+    gvd "$SCR/centos-scripts/$1"
 }
 
 function gvfgi {
-    gvd "$FGI"
+    gvd "$FGI/$1"
 }
 
 function gvgn {
-    gvd "$SCR/GNU_Octave"
+    gvd "$SCR/GNU_Octave/$1"
 }
 
 function gvpy {
-    gvd "$SCR/python-scripts"
+    gvd "$SCR/python-scripts/$1"
 }
+
+function gvlu {
+    gvd "$SCR/lua-scripts/$1"
+}
+
+function gvjs {
+    gvd "$SCR/JScripts/$1"
--- Shell/vim/04-github.sh
+++ Shell/vim/04-github.sh
@@ -1,15 +1,19 @@
-function gvcs {
-    vd "$SCR/centos-scripts"
+function vcs {
+    vd "$SCR/centos-scripts/$1"
 }
 
-function gvfgi {
-    vd "$FGI"
+function vfgi {
+    vd "$FGI/$1"
 }
 
-function gvgn {
-    vd "$SCR/GNU_Octave"
+function vgn {
+    vd "$SCR/GNU_Octave/$1"
 }
 
-function gvpy {
-    vd "$SCR/python-scripts"
+function vpy {
+    vd "$SCR/python-scripts/$1"
+}
+
+function vlu {
+    vd "$SCR/lua-scripts/$1"
 }
