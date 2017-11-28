15b5f8f9925b9b8ece60afef55b2c307cd35fc76
fusion809 <brentonhorne77@gmail.com>
2017-11-29 05:24:18 +1000

    Adding new and improved AUR and SSH git scripts

--- Shell/git/03-ssh.sh
+++ Shell/git/03-ssh.sh
@@ -48,10 +48,7 @@ alias gits=gitsw
 #############################################################
 # Sign in with SSH at startup
 # Makes contributing to GitHub projects a lot simpler.
-if [[ -a $HOME/.ssh/environment ]]
-then
-  SSH_ENV=$HOME/.ssh/environment
-elif [[ $USER == fusion809 ]]
+if [[ $USER == fusion809 ]] && ! [[ -f $HOME/.ssh/id_rsa.pub ]]
 then
   ssh-keygen -t rsa -b 4096 -C "brentonhorne77@gmail.com"
   SSH_ENV=$HOME/.ssh/environment
--- Shell/git/08-aur.sh
+++ Shell/git/08-aur.sh
@@ -1,3 +1,7 @@
+if ! [[ -f $HOME/.ssh/aur.pub ]]; then
+    ssh-keygen -f $HOME/.ssh/aur
+fi
+
 function gitaur {
      git clone ssh://aur@aur.archlinux.org/$1.git
 }
