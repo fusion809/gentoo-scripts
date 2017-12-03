8d6c7956d0c643f851802ff722caf5a3ccaf5740
fusion809 <brentonhorne77@gmail.com>
2017-12-03 16:03:37 +1000

    Adding battery function based on corresponding function in arch-scripts

--- Shell/programs/14-chroot.sh
+++ Shell/programs/14-chroot.sh
@@ -3,16 +3,16 @@ function aroot {
 }
 
 function groot {
-    if ! [[ -f /gentoo-runit/proc/config.gz ]]; then
-        sudo mount -t proc /proc /gentoo-runit/proc
-        sudo mount --rbind /dev /gentoo-runit/dev
-        sudo mount --make-rslave /gentoo-runit/dev
-        sudo mount --rbind /sys /gentoo-runit/sys
-        sudo mount --make-rslave /gentoo-runit/sys
-        sudo rm /gentoo-runit/etc/resolv.conf
-        sudo cp /etc/resolv.conf /gentoo-runit/etc
-        sudo chroot /gentoo-runit /usr/local/bin/md
+    if ! [[ -f /gentoo/proc/config.gz ]]; then
+        sudo mount -t proc /proc /gentoo/proc
+        sudo mount --rbind /dev /gentoo/dev
+        sudo mount --make-rslave /gentoo/dev
+        sudo mount --rbind /sys /gentoo/sys
+        sudo mount --make-rslave /gentoo/sys
+        sudo rm /gentoo/etc/resolv.conf
+        sudo cp /etc/resolv.conf /gentoo/etc
+        sudo chroot /gentoo /usr/local/bin/md
     fi
 
-    sudo chroot /gentoo-runit /usr/local/bin/su-fusion809
+    sudo chroot /gentoo /usr/local/bin/su-fusion809
 }
