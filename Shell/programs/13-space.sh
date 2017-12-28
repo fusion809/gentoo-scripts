20df8f7dbf5b2c44ae9c769b2d4efe63580571a7
fusion809 <brentonhorne77@gmail.com>
2017-12-28 14:50:52 +1000

    Modifying space func to make it work for both Btrfs and ext4

--- Shell/programs/13-space.sh
+++ Shell/programs/13-space.sh
@@ -1,3 +1,8 @@
-function space {
-    df -h /
+function space {]
+    FS=$(mount | grep " / " | sed 's|.*\/ type ||g' | cut -d ' ' -f 1)
+    if [[ $FS == "btrfs" ]]; then
+         btrfs fi df /
+    else
+         df -h /
+    fi
 }
