01af0961ea6c56a799d6f45890b242d31e868c0e
fusion809 <brentonhorne77@gmail.com>
2018-06-18 14:19:41 +1000

    Adding nm funcs, so with my present mask situation I can update without changing them

--- Shell/pms/01-emerge.sh
+++ Shell/pms/01-emerge.sh
@@ -6,3 +6,11 @@ done
 function emupr {
     emup && emrm
 }
+
+function emupnm {
+    sudo emerge --autounmask-keep-masks --verbose-conflicts --keep-going -uDU --with-bdeps=y @world
+}
+
+function syncupnm {
+    sync && emupnm
+}
