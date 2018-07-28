e9ab30d437fbc0eca707a6f3502c678187d5e03c
Brenton Horne <brentonhorne77@gmail.com>
2018-07-29 03:55:02 +1000

    Oops I was editing ovimup in last edit, making it actually vimup now

--- Shell/pms/ebuild/vim.sh
+++ Shell/pms/ebuild/vim.sh
@@ -5,11 +5,6 @@ function ovimup {
     patchversion=$(echo $pkgver | sed "s/$baseversion//g" | sed 's/\.//g')
     vim_baseversion=$(cat vim.spec | grep "%define.*baseversion" | sed 's/%define.*baseversion\s*//g' | head -n 1)
     vim_patchversion=$(cat vim.spec | grep "%define.*patchlevel" | sed 's/%define.*patchlevel\s*//g' | head -n 1)
-    pkgpver=${vim_baseversion}.${vim_patchversion}
-
-    if [[ $pkgver == $pkgpver ]]; then
-         printf "Vim is up-to-date!\n" && exit
-    fi
 
     if [[ $baseversion != $vim_baseversion ]]; then
 
@@ -180,6 +175,18 @@ function vimupb {
 
 # Update Vim in OBS, fusion809-overlay and AUR
 function vimup {
+    cdobsh vim
+    pkgver=$(wget -q https://github.com/vim/vim/releases -O - | grep "tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/v//g' | sed 's/\.tar\.gz//g')
+    baseversion=$(echo $pkgver | sed 's/\.[0-9]*$//g')
+    patchversion=$(echo $pkgver | sed "s/$baseversion//g" | sed 's/\.//g')
+    vim_baseversion=$(cat vim.spec | grep "%define.*baseversion" | sed 's/%define.*baseversion\s*//g' | head -n 1)
+    vim_patchversion=$(cat vim.spec | grep "%define.*patchlevel" | sed 's/%define.*patchlevel\s*//g' | head -n 1)
+    pkgpver=${vim_baseversion}.${vim_patchversion}
+
+    if [[ $pkgver == $pkgpver ]]; then
+         printf "Vim is up-to-date!\n" && exit
+    fi
+
     vimupb
 
     printf '\e[1;34m%-0s\e[m' "Running ovimup vim."
